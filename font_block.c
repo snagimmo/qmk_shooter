1fd950c88db395db7b24742a36231b73abbb0fc5
snagimmo <31765952+snagimmo@users.noreply.github.com>
2021-09-19 20:19:49 +0900

    modified font_block.c

--- font_block.c
+++ font_block.c
@@ -1,24 +1,27 @@
 #include QMK_KEYBOARD_H
 #include "font_block.h"
 
-static uint8_t buf_blocks = 0;
+static uint8_t hrzn_blocks = 0;
+static uint8_t vert_blocks = 0;
 static uint8_t offset = 0;
 
-void set_rotation(uint8_t on) {
-    if (on) {
+void set_rotation(uint8_t isVertical) {
+    if (isVertical) {
         // vertical
-        buf_blocks = OLED_DISPLAY_HEIGHT / OLED_FONT_WIDTH; // 5 (default)
-        offset     = OLED_DISPLAY_HEIGHT % OLED_FONT_WIDTH; // 2px (default)
+        hrzn_blocks = OLED_DISPLAY_HEIGHT / OLED_FONT_WIDTH;  // 5 blocks (128 x 32)
+        offset      = OLED_DISPLAY_HEIGHT % OLED_FONT_WIDTH;  // 2px (128 x 32)
+        vert_blocks = OLED_DISPLAY_WIDTH  / OLED_FONT_HEIGHT; // 16 blocks (128 x 32)
     } else {
         // horizontal
-        buf_blocks = OLED_DISPLAY_WIDTH / OLED_FONT_WIDTH; // 21 (default)
-        offset     = OLED_DISPLAY_WIDTH % OLED_FONT_WIDTH; // 2px (default)
+        hrzn_blocks = OLED_DISPLAY_WIDTH  / OLED_FONT_WIDTH;  // 21 blocks (128 x 32)
+        offset      = OLED_DISPLAY_WIDTH  % OLED_FONT_WIDTH;  // 2px (128 x 32)
+        vert_blocks = OLED_DISPLAY_HEIGHT / OLED_FONT_HEIGHT; // 4 blocks (128 x 32)
     }
 }
 
 void write_font_block(const unsigned char* p, uint8_t rIdx, uint8_t wIdx) {
     unsigned char raw_byte;
-    uint8_t line_number = wIdx / buf_blocks; // Get a line number to write
+    uint8_t line_number = wIdx / hrzn_blocks; // Get a line number to write
 
     for (int i = 0; i < OLED_FONT_WIDTH; i++) {
         raw_byte = pgm_read_byte(p + (rIdx * OLED_FONT_WIDTH + i));
@@ -31,9 +34,9 @@ void write_font_blocks(const unsigned char* p, uint8_t row, uint8_t col, uint8_t
 
     for (int i = 0; i < row; i++) {
         // Read blocks per line from OLED font (default 32 blocks)
-        rIdx  = p_rIdx + (i * 32);
+        rIdx = p_rIdx + (i * 32);
         // Write blocks per line to OLED buffer (default 5 or 21 blocks)
-        wIdx = p_wIdx + (i * buf_blocks);
+        wIdx = p_wIdx + (i * hrzn_blocks);
         for (int j = 0; j < col; j++) {
             write_font_block(p, rIdx + j, wIdx + j);
         }
@@ -41,24 +44,22 @@ void write_font_blocks(const unsigned char* p, uint8_t row, uint8_t col, uint8_t
 }
 
 void clear_display(void) {
-    uint8_t row = (buf_blocks == 5 ? 16   // vertical
-                                   :  3); // horizontal
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < buf_blocks; j++) {
+    for (int i = 0; i < vert_blocks; i++) {
+        for (int j = 0; j < hrzn_blocks; j++) {
             for (int k = 0; k < OLED_FONT_WIDTH; k++) {
                 // Skip 2*N px every line
-                // 0 + i * buf_blocks + j = wIdx + j (L36, L38)
-                oled_write_raw_byte(0x00, (i * buf_blocks + j) * OLED_FONT_WIDTH + k + (i * offset));
+                // 0 + i * hrzn_blocks + j = wIdx + j (L39, L41)
+                oled_write_raw_byte(0x00, (i * hrzn_blocks + j) * OLED_FONT_WIDTH + k + (i * offset));
             }
         }
     }
 }
 
 void clear_block(uint8_t wIdx) {
-    uint8_t row = wIdx / buf_blocks; // Get a row number to write
-    uint8_t col = wIdx % buf_blocks; // Get a column number to write
+    uint8_t row = wIdx / hrzn_blocks; // Get a row number to write
+    uint8_t col = wIdx % hrzn_blocks; // Get a column number to write
 
     for (int i = 0; i < OLED_FONT_WIDTH; i++) {
-        oled_write_raw_byte(0x00, (row * buf_blocks + col) * OLED_FONT_WIDTH + i + (row * offset)); // Skip 2*N px every line
+        oled_write_raw_byte(0x00, (row * hrzn_blocks + col) * OLED_FONT_WIDTH + i + (row * offset)); // Skip 2*N px every line
     }
 }
--- font_block.h
+++ font_block.h
@@ -1,7 +1,7 @@
 #ifndef FONT_BLOCK_H
 #define FONT_BLOCK_H
 
-extern void set_rotation(uint8_t on);
+extern void set_rotation(uint8_t isVertical);
 extern void write_font_block(const unsigned char* p, uint8_t rIdx, uint8_t wIdx);
 extern void write_font_blocks(const unsigned char* p, uint8_t row, uint8_t col, uint8_t p_rIdx, uint8_t p_wIdx);
 extern void clear_display(void);
